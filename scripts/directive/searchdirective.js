009ad8e32d1d8f2807a4a77cdf4dbdce823e1a85
superman90 <liwentao90@yahoo.com>
2016-06-30 21:07:12 -0400

    create bogus filter

--- scripts/directive/searchdirective.js
+++ scripts/directive/searchdirective.js
@@ -9,6 +9,21 @@ angular.module('searchmodule')
             controller:'maincontroller',
             scope:{},
             link:function(scope, element, attrs, controller, transcludeFn) {
+
+                function bogusFilter(data,rawstr){
+                    var sorted = data.sort();
+                    var strs=rawstr.split(" ");
+                    var filterd = sorted.filter(function(value,index,arr){
+                        for (var i=0; i< strs.length;i++){
+                            if (value.trim().toLowerCase().search(strs[i].trim()) > -1){
+                                return true;
+                            }
+                        }
+                        return false;
+                    });
+                    return filterd;
+                }
+
                 scope.$watch("rawSearchStr",function(newstr,oldstr){
                     if (!newstr || /^\s*$/.test(newstr)){
                         scope.searchResult=[];
@@ -16,11 +31,11 @@ angular.module('searchmodule')
                     }else{
                         DoSearchService.rawSearch(newstr).then(
                             function(resp){
-                                scope.searchResult=resp.data.data;
-                                if (resp.data.data.length>0){
-                                    $scope.searchliststyle={};
+                                scope.searchResult=bogusFilter(resp.data.data,newstr);
+                                if (scope.searchResult.length>0){
+                                    scope.searchliststyle={};
                                 }else{
-                                    $scope.searchliststyle={'display':'none'};
+                                    scope.searchliststyle={'display':'none'};
                                 }
                             },
                             function(resp){
